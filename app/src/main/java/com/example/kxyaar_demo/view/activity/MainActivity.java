24011393418d36eeaaf0a2437d8ff860408473e6
dong_shuai_wen <dongshuaiwenchina@163.com>
2022-10-18 18:18:07 +0800

    aar架包使用：调用扫码api，并引入相关依赖

--- app/src/main/java/com/example/kxyaar_demo/view/activity/MainActivity.java
+++ app/src/main/java/com/example/kxyaar_demo/view/activity/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.kxyaar_demo.view.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.coolcollege.aar.bean.NativeEventParams;
+import com.coolcollege.aar.callback.KXYCallback;
+import com.coolcollege.aar.module.APIModule;
+import com.coolcollege.aar.selector.MediaSelector;
 import com.example.kxyaar_demo.R;
+import com.google.gson.Gson;
+
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView textView;
+    private TextView textView; // api调用展示框
+    private String accessToken = "43707c097bdd4302a8b745d72b7381b8"; // 测试token（实际由前端调用方传递）
+    private String enterpriseId = "1324923316665978965"; // 测试企业id（实际由前端调用方传递）
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,10 +69,47 @@ public class MainActivity extends AppCompatActivity {
     public void click_scan(View view) {
         Log.d("TAG", "click_scan: " + "扫码");
         textView.setText("扫码");
+
+        NativeEventParams params = new NativeEventParams();
+        params.methodName = "scan";
+        params.methodData = "{}";
+        callModule(params);
     }
     // 获取位置信息
     public void click_getLocation(View view) {
         Log.d("TAG", "click_getLocation: " + "获取位置信息");
         textView.setText("获取位置信息");
+    }
+
+    // 统一调用API
+    public void callModule(NativeEventParams params) {
+        APIModule.getAPIModule(this).moduleManage(params, accessToken, enterpriseId, 123, new KXYCallback() {
+            // aar 无需跳转界面的api回调
+            @Override
+            public void onOKCallback(Object o) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        textView.setText(new Gson().toJson(o));
+                    }
+                });
+            }
+
+            @Override
+            public void onErrorCallback(Object o) {
+                textView.setText(new Gson().toJson(o));
+            }
+        });
+    }
+
+    @Override
+    // aar 需跳转界面的api回调
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (data == null) return;
+        String text = new Gson().toJson(data.getParcelableExtra(MediaSelector.RESULT_DATA) != null ? data.getParcelableExtra(MediaSelector.RESULT_DATA) : data.getParcelableArrayListExtra(MediaSelector.RESULT_DATA));
+        text = (text != null && !("null".equals(text)))?text:data.getStringExtra(MediaSelector.RESULT_DATA);
+        textView.setText(text);
     }
 }
